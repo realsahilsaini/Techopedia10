256d3a02b6e809cd7af4bca1c682e1718c512637
Shruti Tiwari <sh03shruti@gmail.com>
2022-02-02 22:53:52 +0530

    nexus link

--- forms/script.js
+++ forms/script.js
@@ -267,7 +267,7 @@ let serialJSON = {};
 function sendData(event) {
   serialJSON["event"] = event;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbxROJq-2qbNnvKOLOxZLXs9SyM0y8Qp2MzXBeVEqZYO5bROafBtEyjnzIkjHqHhEIDR-g/exec",
+    url: "https://script.google.com/macros/s/AKfycby4xNC63sPp-g61ZUO5_g9yfUYYowATbX-hGfPn2gd4kxDvG8HdgcbxwtUWy7eJ9M8LoA/exec",
 
     type: "POST",
     data: serialJSON,
@@ -297,7 +297,7 @@ function sendData(event) {
 
 window.onload = function () {
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbxROJq-2qbNnvKOLOxZLXs9SyM0y8Qp2MzXBeVEqZYO5bROafBtEyjnzIkjHqHhEIDR-g/exec",
+    url: "https://script.google.com/macros/s/AKfycby4xNC63sPp-g61ZUO5_g9yfUYYowATbX-hGfPn2gd4kxDvG8HdgcbxwtUWy7eJ9M8LoA/exec",
     type: "GET",
     dataType: "json",
 
